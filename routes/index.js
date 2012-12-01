8d67f3bc28cdddf753cb58a6e25ab93f73cb2326
Gabriel Farrell <g@grrawr.com>
2012-12-01 17:28:51 -0500

    Added test csv route

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,18 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index');
+};
+
+/*
+ * GET csv page.
+ */
+
+exports.csv = function(req, res){
+  res.writeHead(200, {
+    'Content-Disposition': 'attachment; filename="test.csv"',
+    'Content-Type': 'text/csv'
+  })
+  res.write('Thing1,Thing2,Thing3\na,b,c\nd,e,f')
+  res.end()
 };
