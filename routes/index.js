1b87e2c83b1cbf6507beb42c7177f44283cadc57
Mike Ball <mikedball@gmail.com>
2012-12-02 13:55:31 -0500

    Almost complete CSV-building functionality

--- routes/index.js
+++ routes/index.js
@@ -20,15 +20,19 @@ exports.api = function(req, res){
 
   var options = {};
 
-  if (req.query.preview) options.top = 5;
+  //if (req.query.preview) options.top = 5;
+  options.top = 5
   options.zip = req.query.zip
 
   phli.getType(req.query.type, options, function (err, data) {
+    var csvHeadings;
+
     if (err) {
       res.writeHead(400)
       return res.end()
     }
 
+    csvHeadings = buildHeadingsList(data.d.results[0]);
 
     if (req.query.preview) {  // just send our bit of JSON
       res.writeHead(200, {'Content-Type': 'application/json'});
@@ -40,8 +44,54 @@ exports.api = function(req, res){
       'Content-Disposition': 'attachment; filename="test.csv"',
       'Content-Type': 'text/csv'
     });
-    // TODO make it csv
-    res.write(JSON.stringify(data))
+    buildCSV(res, csvHeadings, data)
     res.end()
-  })
+  });
 };
+
+function buildHeadingsList(item) {
+  var headings = [];
+  var key;
+  var locKey;
+  var locValue;
+  var value;
+
+  headings = flatten(item, true);
+
+  return headings;
+}
+
+function buildCSV(res, headings, data) {
+  var results = data.d.results;
+  var row;
+  
+  res.write('"' + headings.join('","') + '"\n');
+  results.forEach(function(result) {
+    row = flatten(result);
+    res.write('"' + row.join('","') + '"\n');
+  });
+}
+
+function flatten(result, useKeys) {
+  var key;
+  var locKey;
+  var locValue;
+  var value;
+  var row = [];
+ 
+  for (key in result) {
+    value = result[key];
+    if (key === 'locations') {
+      for (locKey in key) {
+        locValue = value[locKey];
+        if (typeof locValue === 'object') continue;
+        row.push(useKeys ? locKey : locValue);
+      }
+      continue;
+    }
+    if (typeof value === "object") continue;
+    row.push(useKeys ? key : value);
+  }
+
+  return row;
+}
